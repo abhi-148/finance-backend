d085dab49472d5d3cc013b353e2677b29f04bc8c
abhi-148 <your-email@example.com>
2026-04-03 00:43:40 +0530

    Fix npm ci error

--- server.js
+++ server.js
@@ -6,24 +6,24 @@ const cors = require("cors");
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Routes import
+
 const userRoutes = require("./routes/userRoutes");
 const recordRoutes = require("./routes/recordRoutes");
 
-// Routes use
+
 app.use("/api/users", userRoutes);
 app.use("/api/records", recordRoutes);
 
-// Root route
+
 app.get("/", (req, res) => {
   res.send("Finance Backend Running 🚀");
 });
 
-// MongoDB connection
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
@@ -34,7 +34,6 @@ const connectDB = async () => {
   }
 };
 
-// Start server AFTER DB connects (important for deploy)
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
